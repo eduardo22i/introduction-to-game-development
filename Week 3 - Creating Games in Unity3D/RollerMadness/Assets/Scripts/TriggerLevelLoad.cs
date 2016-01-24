4252ba2d5b93cb3fd5752525b9ef5d1d4325d6aa
Eduardo Irías <eduardoirias@me.com>
2016-01-24 13:13:55 -0600

    Project 2: Pickups, UI, Game Manager, and Enemies
    Roller Madness - Project 2: Pickups, UI, Game Manager, and Enemies

---
+++ Week 3 - Creating Games in Unity3D/RollerMadness/Assets/Scripts/TriggerLevelLoad.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLevelLoad : MonoBehaviour {
+
+	public string nameOfLevelToLoad  = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Player" )
+		{
+			Application.LoadLevel(nameOfLevelToLoad);
+		}
+	}
+}
---
+++ Week 3 - Creating Games in Unity3D/RollerMadness/Assets/Scripts/UIButtonLevelLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UIButtonLevelLoad : MonoBehaviour {
+	
+	public string LevelToLoad;
+	
+	public void loadLevel() {
+		//Load the level from LevelToLoad
+		Application.LoadLevel(LevelToLoad);
+	}
+}
